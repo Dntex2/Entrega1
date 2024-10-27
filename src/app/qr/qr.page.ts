02d1b617a6a1f2a4ff9c6562df94e0bb4b174b6b
Eduardo Andres Guzman Manquehual <131813329+eg-andreszx@users.noreply.github.com>
2024-10-27 19:39:23 -0300

    eduQR
    tuvo unos cambios, a la hora de lo estético, se buscó agrandar el espacio de la imagen,. pero no se encontró solución, tiene un fallo el cual se planea corregir a priori

--- src/app/qr/qr.page.ts
+++ src/app/qr/qr.page.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { CapacitorBarcodeScanner, CapacitorBarcodeScannerTypeHint, CapacitorBarcodeScannerTypeHintALLOption } from '@capacitor/barcode-scanner';
+import { Component } from '@angular/core';
+import { CapacitorBarcodeScanner, CapacitorBarcodeScannerTypeHint } from '@capacitor/barcode-scanner';
 
 @Component({
   selector: 'app-qr',
@@ -7,18 +7,17 @@ import { CapacitorBarcodeScanner, CapacitorBarcodeScannerTypeHint, CapacitorBarc
   styleUrls: ['./qr.page.scss'],
 })
 export class QRPage  {
+  result: string = '';
+  isScanning: boolean = false;
 
-  result: string = ''
-
-  constructor() { }
-
-
+  constructor() {}
 
   async scan(): Promise<void> {
+    this.isScanning = true; // Activa la vista de la cámara
     const result = await CapacitorBarcodeScanner.scanBarcode({
       hint: CapacitorBarcodeScannerTypeHint.ALL
     });
     this.result = result.ScanResult;
+    this.isScanning = false; // Desactiva la vista de la cámara al terminar
   }
-
 }
