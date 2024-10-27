0b9655d57af13e5f54b51ce1f39bb3fba9023e91
David Martinez <131811698+Dntex2@users.noreply.github.com>
2024-10-27 15:34:14 -0300

    Pagina-home-Guards
    Se agrega exitosamente un guard, se puede ver reflejado en el check de "Recordas mis credenciales".

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,15 +9,24 @@ export class HomePage {
   username: string = '';
   password: string = '';
   errorMessage: string = '';
+  rememberMe: boolean = false;
 
 
 
-  constructor(private router: Router) {}
+  constructor(private router: Router) {
+    const isLoggedIn = localStorage.getItem('isLoggedIn');
+    if (isLoggedIn){
+      this.router.navigate(['/inicio'])
+    }
+  }
   onSubmit() {
     const storedUsername = localStorage.getItem('username');
     const storedPassword = localStorage.getItem('password');
 
     if (this.username === storedUsername && this.password === storedPassword) {
+      if (this.rememberMe){
+        localStorage.setItem('isLoggedIn', 'true');
+      }
       this.router.navigate(['/inicio']); 
     } else {
       this.errorMessage = 'Credenciales invalidas. Intentalo de nuevo.';
